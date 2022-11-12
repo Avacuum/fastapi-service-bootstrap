69ba8b6f8e1177f5335987a1f9ab7ef895ecaac8
abu <abu@abu.abu>
2022-11-13 05:47:37 +0600

    add requirements

--- main.py
+++ main.py
@@ -1,28 +1,25 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from settings import settings
-
+from api import router
 
 tags_metadata = [
     {
-        'name': 'text',
-        'description': 'text content for landing page'
+        'name': 'bootstrap api',
+        'description': 'example for bootrstrap'
     }
 ]
 
 app = FastAPI(
-    title='Workaus Content Service',
-    description='Gives text and media content to frontend',
+    title='Bootstrap',
+    description='bootstrap for FastAPI',
     version='1.0.0',
     openapi_tags=tags_metadata,
 )
-
-origins = []
-origins.extend(settings.allowed_urls.split(','))
-
+print(settings.allowed_urls)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=[],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
